f98ebf36f14dee699c81c9d8f198620bf68b793f
Tanguy Leconte <taleconte@gmail.com>
2023-03-17 15:01:49 +0100

    Add a clear button

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,38 @@
 const grid = document.querySelector('.grid');
-let numberOfSquares = 16;
+let numberOfSquares = 32//prompt("How many squares ? (Max: 100)");
+if(numberOfSquares > 100 || numberOfSquares < 1){
+    numberOfSquares = 32;
+}
+displayGrid(numberOfSquares);
 
-
-for(let i = 0; i < 16; i++){
-    for(let j = 0; j < numberOfSquares; j++){
+function displayGrid(numberOfSquares){
+    for(let i = 0; i < numberOfSquares; i++){
+        for(let j = 0; j < numberOfSquares; j++){
             const square = document.createElement('div');
             square.classList.add('square');
-            square.style.width = (500/16)-2+'px';
-            square.style.height = (500/16)-2+'px';
+            square.style.width = (600/numberOfSquares)+'px';
+            square.style.height = (600/numberOfSquares)+'px';
             grid.appendChild(square);
+        }
     }
+
 }
 
 
+let clearButton = document.querySelector('.clearGrid');
+clearButton.addEventListener('click', () => {
+    for(let i = squares.length ; i > 0; i--){
+        grid.removeChild(grid.lastChild); 
+    }
+    displayGrid(numberOfSquares);
+})
+
+function colorSquares(){
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'red';
+        }  )
+    })
+}
 
